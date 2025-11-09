9c1f9657b0162a4c6945f9e2f7917f93974d4458
Urjit Sharma <urjitsharma12@gmail.com>
2025-11-08 17:28:12 -0800

    Implemented test cases for Milestone 1

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "protocol.h"
-#include "encryption.h"
-#include "routing.h"
+// #include "encryption.h"
+// #include "routing.h"
 
 int main(int argc, char * argv[]) {
     printf("%lu", sizeof(struct header));
